0d56bc95fd128dabde08f102a55779aa16cfede8
pavelvazquez <pavelva@uio.no>
2025-06-24 14:45:36 +0200

    Centralise defaults in application.yml
    (cherry picked from commit 4ef10ef7a9b7d757af63bebee43b6e6656a459d5)

--- services/localega-tsd-proxy/src/main/java/no/elixir/fega/ltp/LocalEGATSDProxyApplication.java
+++ services/localega-tsd-proxy/src/main/java/no/elixir/fega/ltp/LocalEGATSDProxyApplication.java
@@ -50,10 +50,10 @@ import org.springframework.web.client.RestTemplate;
 @EnableWebSecurity
 public class LocalEGATSDProxyApplication {
 
-  @Value("${token.redirect-uri:{baseUrl}/oidc-protected}")
+  @Value("${token.redirect-uri}")
   private String redirectUri;
 
-  @Value("${aai.service-base-url:https://login.elixir-czech.org}")
+  @Value("${aai.service-base-url}")
   private String aaiBase;
 
   public static void main(String[] args) {
