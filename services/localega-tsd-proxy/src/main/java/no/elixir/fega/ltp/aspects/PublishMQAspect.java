fdb33ac6a6433bf0c9e235e6440c446d5886f1a7
Yasin <wytm97@protonmail.com>
2025-06-27 13:56:00 +0200

    test: add debug logs to publish aspect

--- services/localega-tsd-proxy/src/main/java/no/elixir/fega/ltp/aspects/PublishMQAspect.java
+++ services/localega-tsd-proxy/src/main/java/no/elixir/fega/ltp/aspects/PublishMQAspect.java
@@ -1,11 +1,7 @@
 package no.elixir.fega.ltp.aspects;
 
-import static no.elixir.fega.ltp.aspects.ProcessArgumentsAspect.*;
-
 import com.google.gson.Gson;
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.Objects;
-import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import no.elixir.fega.ltp.dto.EncryptedIntegrity;
 import no.elixir.fega.ltp.dto.FileDescriptor;
@@ -21,138 +17,151 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
-/** AOP aspect that publishes MQ messages. */
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.UUID;
+
+import static no.elixir.fega.ltp.aspects.ProcessArgumentsAspect.*;
+
+/**
+ * AOP aspect that publishes MQ messages.
+ */
 @Slf4j
 @Aspect
 @Order(4)
 @Component
 public class PublishMQAspect {
 
-  private final HttpServletRequest request;
+    private final HttpServletRequest request;
 
-  private final Gson gson;
+    private final Gson gson;
 
-  private final RabbitTemplate tsdRabbitTemplate;
+    private final RabbitTemplate tsdRabbitTemplate;
 
-  @Value("${tsd.project}")
-  private String tsdProjectId;
+    @Value("${tsd.project}")
+    private String tsdProjectId;
 
-  @Value("${tsd.inbox-location}")
-  private String tsdInboxLocation;
+    @Value("${tsd.inbox-location}")
+    private String tsdInboxLocation;
 
-  @Value("${mq.tsd.exchange}")
-  private String exchange;
+    @Value("${mq.tsd.exchange}")
+    private String exchange;
 
-  @Value("${mq.tsd.inbox-routing-key}")
-  private String routingKey;
+    @Value("${mq.tsd.inbox-routing-key}")
+    private String routingKey;
 
-  @Autowired
-  public PublishMQAspect(HttpServletRequest request, Gson gson, RabbitTemplate tsdRabbitTemplate) {
-    this.request = request;
-    this.gson = gson;
-    this.tsdRabbitTemplate = tsdRabbitTemplate;
-  }
-
-  /**
-   * Publishes <code>FileDescriptor</code> to the MQ upon file uploading.
-   *
-   * @param result Object returned by the proxied method.
-   */
-  @SuppressWarnings({"unchecked", "rawtypes"})
-  @AfterReturning(
-      pointcut =
-          "execution(@org.springframework.web.bind.annotation.PatchMapping public * no.elixir.fega.ltp.controllers.rest.ProxyController.stream(..))",
-      returning = "result")
-  public void publishUpload(Object result) {
-    ResponseEntity genericResponseEntity = (ResponseEntity) result;
-    if (!String.valueOf(Objects.requireNonNull(genericResponseEntity).getStatusCode())
-        .startsWith("20")) {
-      log.error(String.valueOf(genericResponseEntity.getStatusCode()));
-      log.error(String.valueOf(genericResponseEntity.getBody()));
-      return;
-    }
-    ResponseEntity<TSDFileAPIResponse> tsdResponseEntity =
-        (ResponseEntity<TSDFileAPIResponse>) result;
-    TSDFileAPIResponse body = tsdResponseEntity.getBody();
-    if (!String.valueOf(Objects.requireNonNull(body).getStatusCode()).startsWith("20")) {
-      log.error(String.valueOf(body.getStatusCode()));
-      log.error(String.valueOf(body.getStatusText()));
-      return;
+    @Autowired
+    public PublishMQAspect(HttpServletRequest request, Gson gson, RabbitTemplate tsdRabbitTemplate) {
+        this.request = request;
+        this.gson = gson;
+        this.tsdRabbitTemplate = tsdRabbitTemplate;
     }
 
-    if (!"end".equalsIgnoreCase(String.valueOf(request.getAttribute(CHUNK)))) {
-      return;
+    /**
+     * Publishes <code>FileDescriptor</code> to the MQ upon file uploading.
+     *
+     * @param result Object returned by the proxied method.
+     */
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    @AfterReturning(
+            pointcut =
+                    "execution(@org.springframework.web.bind.annotation.PatchMapping public * no.elixir.fega.ltp.controllers.rest.ProxyController.stream(..))",
+            returning = "result")
+    public void publishUpload(Object result) {
+        ResponseEntity genericResponseEntity = (ResponseEntity) result;
+        if (!String.valueOf(Objects.requireNonNull(genericResponseEntity).getStatusCode())
+                .startsWith("20")) {
+            log.error(String.valueOf(genericResponseEntity.getStatusCode()));
+            log.error(String.valueOf(genericResponseEntity.getBody()));
+            return;
+        }
+        ResponseEntity<TSDFileAPIResponse> tsdResponseEntity =
+                (ResponseEntity<TSDFileAPIResponse>) result;
+        TSDFileAPIResponse body = tsdResponseEntity.getBody();
+        if (!String.valueOf(Objects.requireNonNull(body).getStatusCode()).startsWith("20")) {
+            log.error(String.valueOf(body.getStatusCode()));
+            log.error(String.valueOf(body.getStatusText()));
+            return;
+        }
+
+        if (!"end".equalsIgnoreCase(String.valueOf(request.getAttribute(CHUNK)))) {
+            return;
+        }
+
+        FileDescriptor fileDescriptor = new FileDescriptor();
+        fileDescriptor.setUser(request.getAttribute(EGA_USERNAME).toString());
+        String fileName = request.getAttribute(FILE_NAME).toString();
+        fileDescriptor.setFilePath(
+                String.format(tsdInboxLocation, tsdProjectId, request.getAttribute(ELIXIR_ID).toString())
+                        + fileName); // absolute path to the file
+        fileDescriptor.setFileSize(Long.parseLong(request.getAttribute(FILE_SIZE).toString()));
+        fileDescriptor.setFileLastModified(System.currentTimeMillis() / 1000);
+        fileDescriptor.setOperation(Operation.UPLOAD.name().toLowerCase());
+        fileDescriptor.setEncryptedIntegrity(
+                new EncryptedIntegrity[]{
+                        new EncryptedIntegrity(SHA256.toLowerCase(), request.getAttribute(SHA256).toString())
+                });
+        publishMessage(fileDescriptor, Operation.UPLOAD.name().toLowerCase());
     }
 
-    FileDescriptor fileDescriptor = new FileDescriptor();
-    fileDescriptor.setUser(request.getAttribute(EGA_USERNAME).toString());
-    String fileName = request.getAttribute(FILE_NAME).toString();
-    fileDescriptor.setFilePath(
-        String.format(tsdInboxLocation, tsdProjectId, request.getAttribute(ELIXIR_ID).toString())
-            + fileName); // absolute path to the file
-    fileDescriptor.setFileSize(Long.parseLong(request.getAttribute(FILE_SIZE).toString()));
-    fileDescriptor.setFileLastModified(System.currentTimeMillis() / 1000);
-    fileDescriptor.setOperation(Operation.UPLOAD.name().toLowerCase());
-    fileDescriptor.setEncryptedIntegrity(
-        new EncryptedIntegrity[] {
-          new EncryptedIntegrity(SHA256.toLowerCase(), request.getAttribute(SHA256).toString())
-        });
-    publishMessage(fileDescriptor, Operation.UPLOAD.name().toLowerCase());
-  }
+    /**
+     * Publishes <code>FileDescriptor</code> to the MQ upon file removal.
+     *
+     * @param result Object returned by the proxied method.
+     */
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    @AfterReturning(
+            pointcut =
+                    "execution(public * no.elixir.fega.ltp.controllers.rest.ProxyController.deleteFile(..))",
+            returning = "result")
+    public void publishRemove(Object result) {
+        try {
+            ResponseEntity genericResponseEntity = (ResponseEntity) result;
+            if (!String.valueOf(Objects.requireNonNull(genericResponseEntity).getStatusCode())
+                    .startsWith("20")) {
+                log.error(String.valueOf(genericResponseEntity.getStatusCode()));
+                log.error(String.valueOf(genericResponseEntity.getBody()));
+                return;
+            }
+            ResponseEntity<TSDFileAPIResponse> tsdResponseEntity =
+                    (ResponseEntity<TSDFileAPIResponse>) result;
+            TSDFileAPIResponse body = tsdResponseEntity.getBody();
+            if (!String.valueOf(Objects.requireNonNull(body).getStatusCode()).startsWith("20")) {
+                log.error(String.valueOf(body.getStatusCode()));
+                log.error(String.valueOf(body.getStatusText()));
+                return;
+            }
 
-  /**
-   * Publishes <code>FileDescriptor</code> to the MQ upon file removal.
-   *
-   * @param result Object returned by the proxied method.
-   */
-  @SuppressWarnings({"unchecked", "rawtypes"})
-  @AfterReturning(
-      pointcut =
-          "execution(public * no.elixir.fega.ltp.controllers.rest.ProxyController.deleteFile(..))",
-      returning = "result")
-  public void publishRemove(Object result) {
-    ResponseEntity genericResponseEntity = (ResponseEntity) result;
-    if (!String.valueOf(Objects.requireNonNull(genericResponseEntity).getStatusCode())
-        .startsWith("20")) {
-      log.error(String.valueOf(genericResponseEntity.getStatusCode()));
-      log.error(String.valueOf(genericResponseEntity.getBody()));
-      return;
+            FileDescriptor fileDescriptor = new FileDescriptor();
+            fileDescriptor.setUser(request.getAttribute(EGA_USERNAME).toString());
+            String fileName = request.getAttribute(FILE_NAME).toString();
+            fileDescriptor.setFilePath(
+                    String.format(tsdInboxLocation, tsdProjectId, request.getAttribute(ELIXIR_ID).toString())
+                            + fileName);
+            fileDescriptor.setOperation(Operation.REMOVE.name().toLowerCase());
+            publishMessage(fileDescriptor, Operation.REMOVE.name().toLowerCase());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            log.error(Arrays.toString(e.getStackTrace()));
+        }
     }
-    ResponseEntity<TSDFileAPIResponse> tsdResponseEntity =
-        (ResponseEntity<TSDFileAPIResponse>) result;
-    TSDFileAPIResponse body = tsdResponseEntity.getBody();
-    if (!String.valueOf(Objects.requireNonNull(body).getStatusCode()).startsWith("20")) {
-      log.error(String.valueOf(body.getStatusCode()));
-      log.error(String.valueOf(body.getStatusText()));
-      return;
+
+    private void publishMessage(FileDescriptor fileDescriptor, String type) {
+        String json = gson.toJson(fileDescriptor);
+        tsdRabbitTemplate.convertAndSend(
+                exchange,
+                routingKey,
+                json,
+                m -> {
+                    m.getMessageProperties().setContentType(ContentType.APPLICATION_JSON.getMimeType());
+                    m.getMessageProperties().setCorrelationId(UUID.randomUUID().toString());
+                    return m;
+                });
+        log.info(
+                "{} message published to {} exchange with routing key {}: {}",
+                type,
+                exchange,
+                routingKey,
+                json);
     }
-
-    FileDescriptor fileDescriptor = new FileDescriptor();
-    fileDescriptor.setUser(request.getAttribute(EGA_USERNAME).toString());
-    String fileName = request.getAttribute(FILE_NAME).toString();
-    fileDescriptor.setFilePath(
-        String.format(tsdInboxLocation, tsdProjectId, request.getAttribute(ELIXIR_ID).toString())
-            + fileName);
-    fileDescriptor.setOperation(Operation.REMOVE.name().toLowerCase());
-    publishMessage(fileDescriptor, Operation.REMOVE.name().toLowerCase());
-  }
-
-  private void publishMessage(FileDescriptor fileDescriptor, String type) {
-    String json = gson.toJson(fileDescriptor);
-    tsdRabbitTemplate.convertAndSend(
-        exchange,
-        routingKey,
-        json,
-        m -> {
-          m.getMessageProperties().setContentType(ContentType.APPLICATION_JSON.getMimeType());
-          m.getMessageProperties().setCorrelationId(UUID.randomUUID().toString());
-          return m;
-        });
-    log.info(
-        "{} message published to {} exchange with routing key {}: {}",
-        type,
-        exchange,
-        routingKey,
-        json);
-  }
 }
