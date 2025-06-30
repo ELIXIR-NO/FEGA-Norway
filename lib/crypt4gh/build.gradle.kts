4ab55f153d38a9feb2cd46369731ee1be1191d27
Parisa <parisa.tejari@uib.no>
2025-06-30 14:36:11 +0200

    add sourcesJar and javadocJar to tool publication

--- lib/crypt4gh/build.gradle.kts
+++ lib/crypt4gh/build.gradle.kts
@@ -205,6 +205,8 @@ publishing {
                 classifier = ""
                 builtBy(tasks.shadowJar)
             }
+            artifact(tasks.named("sourcesJar"))
+            artifact(tasks.named("javadocJar"))
             pom {
                 setupPomDetails()
                 // the pom-file for the fat Jar does not include any dependencies
