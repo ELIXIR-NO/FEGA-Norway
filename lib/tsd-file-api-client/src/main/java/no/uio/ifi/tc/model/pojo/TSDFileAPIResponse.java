37257768cfbe964f661bb63b6639e379788a9bbc
Yasin <wytm97@protonmail.com>
2025-07-02 13:13:03 +0200

    fix: remove jackson annotations

--- lib/tsd-file-api-client/src/main/java/no/uio/ifi/tc/model/pojo/TSDFileAPIResponse.java
+++ lib/tsd-file-api-client/src/main/java/no/uio/ifi/tc/model/pojo/TSDFileAPIResponse.java
@@ -1,6 +1,5 @@
 package no.uio.ifi.tc.model.pojo;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 import lombok.ToString;
@@ -9,11 +8,9 @@ import lombok.ToString;
 @ToString
 public class TSDFileAPIResponse {
 
-  @JsonProperty("statusCode")
   @SerializedName("statusCode")
   private int statusCode;
 
-  @JsonProperty("statusText")
   @SerializedName("statusText")
   private String statusText;
 }
