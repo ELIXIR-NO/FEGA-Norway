72b61f9a20698d85e50c32b6a55608393ea47270
Parisa <parisa.tejari@uib.no>
2025-08-06 12:12:52 +0200

    set Java version to 21

--- lib/crypt4gh/build.gradle.kts
+++ lib/crypt4gh/build.gradle.kts
@@ -15,8 +15,9 @@ group = "no.elixir"
 
 // The target Java version can be overriden on the command-line with the argument "-PjavaVersion=<version>"
 // A JDK of this version must be available on your system
+private val defaultJavaVersion = JavaVersion.VERSION_21.majorVersion.toInt()
 val javaVersion = (project.findProperty("javaVersion") as String?)?.toInt()
-    ?: JavaVersion.current().majorVersion.toInt()
+    ?: defaultJavaVersion
 
 repositories {
     mavenCentral()
