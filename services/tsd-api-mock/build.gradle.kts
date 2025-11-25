8e60fda04e543d44de66b79ff5fdd768f54be34d
Parisa <parisa.tejari@uib.no>
2025-11-25 13:13:47 +0100

    fix not found classes after upgrading to spring-boot 4

--- services/tsd-api-mock/build.gradle.kts
+++ services/tsd-api-mock/build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.13.0")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.20.1")
+    testImplementation("org.springframework.boot:spring-boot-resttestclient")
     implementation("jakarta.validation:jakarta.validation-api")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
