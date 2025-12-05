5194e99f340db3689783fa861fbb5074ab5467a5
Parisa <parisa.tejari@uib.no>
2025-12-05 09:24:23 +0100

    [refactor] refactor path mask method and add names to masked fields

--- services/localega-tsd-proxy/src/main/java/no/elixir/fega/ltp/common/Masker.java
+++ services/localega-tsd-proxy/src/main/java/no/elixir/fega/ltp/common/Masker.java
@@ -1,23 +1,24 @@
 package no.elixir.fega.ltp.common;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class Masker {
 
   public static String maskUsername(String username) {
     try {
-      if (username.length() < 6)
-        return username.replaceAll("(?<=.{2}).(?=.)", "*");
+      if (username.length() < 6) return username.replaceAll("(?<=.{2}).(?=.)", "*");
       else return username.replaceAll("(?<=.{3}).(?=.{2})", "*");
     } catch (Exception e) {
       return username;
     }
   }
-  
+
   public static String maskEmail(String email) {
     try {
       String username = email.substring(0, email.indexOf("@"));
-      //mask the username
-      if (username.length() < 6)
-        email = email.replaceAll("(?<=.{2}).(?=.*.{1}@)", "*");
+      // mask the username
+      if (username.length() < 6) email = email.replaceAll("(?<=.{2}).(?=.*.{1}@)", "*");
       else email = email.replaceAll("(?<=.{3}).(?=.*.{2}@)", "*");
       // mask the domain
       return email.replaceAll("(?<=@)[^.]+(?=\\.[^.]+$)", "*****");
@@ -28,12 +29,14 @@ public final class Masker {
 
   public static String maskEmailInPath(String path) {
     try {
-      String[] partsInPath = path.split("/", -1);
-      String[] partsInEmail = partsInPath[1].split("-", 2);
-      System.out.println(partsInEmail[1]);
-      partsInEmail[1] = maskEmail(partsInEmail[1]);
-      partsInPath[1] = String.join("-", partsInEmail);
-      return String.join("/", partsInPath);
+      String regex = "/([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,})/";
+      Pattern pattern = Pattern.compile(regex);
+      Matcher matcher = pattern.matcher(path);
+      if (matcher.find()) {
+        return matcher.replaceFirst(
+            Matcher.quoteReplacement("/" + maskEmail(matcher.group(1)) + "/"));
+      }
+      return path;
     } catch (Exception e) {
       return path;
     }
