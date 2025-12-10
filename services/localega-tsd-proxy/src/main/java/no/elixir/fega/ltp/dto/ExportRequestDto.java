aa2d58a62cd8eb06d799a65c5c1d4f24e10f9ee7
Yasin <wytm97@protonmail.com>
2025-12-10 10:44:37 +0100

    chore: apply codeformatting

--- services/localega-tsd-proxy/src/main/java/no/elixir/fega/ltp/dto/ExportRequestDto.java
+++ services/localega-tsd-proxy/src/main/java/no/elixir/fega/ltp/dto/ExportRequestDto.java
@@ -17,24 +17,20 @@ import lombok.ToString;
 public class ExportRequestDto {
 
   // This is the user's Passport Scoped Access Token.
-  @NotBlank(message = "The field 'accessToken' must not be blank.")
-  @JsonProperty("accessToken")
+  @NotBlank(message = "The field 'accessToken' must not be blank.") @JsonProperty("accessToken")
   private String accessToken;
 
   // This is the visa token that DOA expects
   @JsonProperty("jwtToken")
   private String jwtToken;
 
-  @NotBlank(message = "The field 'id' must not be blank.")
-  @JsonProperty("id")
+  @NotBlank(message = "The field 'id' must not be blank.") @JsonProperty("id")
   private String id;
 
-  @NotBlank(message = "The field 'userPublicKey' must not be blank.")
-  @JsonProperty("userPublicKey")
+  @NotBlank(message = "The field 'userPublicKey' must not be blank.") @JsonProperty("userPublicKey")
   private String userPublicKey;
 
-  @NotNull(message = "The field 'type' must not be null. Should be either 'fileId' or 'datasetId'.")
-  @JsonProperty("type")
+  @NotNull(message = "The field 'type' must not be null. Should be either 'fileId' or 'datasetId'.") @JsonProperty("type")
   private ExportType type = ExportType.DATASET_ID;
 
   @ToString
@@ -42,8 +38,8 @@ public class ExportRequestDto {
   public enum ExportType {
     FILE_ID("fileId"),
     DATASET_ID("datasetId");
-    @JsonValue
-    private final String value;
+    @JsonValue private final String value;
+
     @JsonCreator
     public static ExportType fromValue(String value) {
       for (ExportType type : ExportType.values()) {
