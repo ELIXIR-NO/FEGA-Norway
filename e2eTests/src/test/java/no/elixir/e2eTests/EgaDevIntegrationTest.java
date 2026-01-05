cb071a4b82c83e30bd7c28c6e5edd855208138b0
Yasin <wytm97@protonmail.com>
2026-01-05 12:48:13 +0100

    feat: e2e test suite for ega-dev env

---
+++ e2eTests/src/test/java/no/elixir/e2eTests/EgaDevIntegrationTest.java
@@ -0,0 +1,68 @@
+package no.elixir.e2eTests;
+
+import no.elixir.e2eTests.core.BaseE2ETest;
+import no.elixir.e2eTests.features.*;
+import no.elixir.e2eTests.utils.CommonUtils;
+import org.junit.jupiter.api.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class EgaDevIntegrationTest {
+
+  @BeforeAll
+  static void setup() throws Exception {
+    BaseE2ETest.setupEgaDevTestEnvironment();
+  }
+
+  @AfterAll
+  static void cleanup() {
+    BaseE2ETest.cleanupTestEnvironment();
+  }
+
+  @Test
+  @Order(1)
+  void UploadTest() throws Exception {
+    UploadTest.uploadThroughProxy();
+    // Wait for triggers to be set up at CEGA.
+    // Not really needed if using local CEGA container.
+    CommonUtils.waitForProcessing(5000);
+  }
+
+  @Test
+  @Order(2)
+  void IngestTest() throws Exception {
+    IngestTest.publishIngestionMessageToCEGA();
+    // Wait for the LEGA ingest and verify services
+    // to complete and update DB.
+    CommonUtils.waitForProcessing(5000);
+  }
+
+  @Test
+  @Order(3)
+  void AccessionTest() throws Exception {
+    AccessionTest.publishAccessionMessageOnBehalfOfCEGAToLocalEGA();
+    // Wait for LEGA finalize service to complete and update DB.
+    CommonUtils.waitForProcessing(5000);
+  }
+
+  @Test
+  @Order(4)
+  void MappingTest() throws Exception {
+    MappingTest.triggerMappingMessageFromCEGA();
+    // Wait for LEGA mapper service to store mapping
+    CommonUtils.waitForProcessing(1000);
+  }
+
+  @Test
+  @Order(5)
+  void ReleaseTest() throws Exception {
+    ReleaseTest.triggerReleaseMessageFromCEGA();
+    // Wait for LEGA mapper service to update dataset status
+    CommonUtils.waitForProcessing(1000);
+  }
+
+  @Test
+  @Order(6)
+  void DownloadTest() throws Exception {
+    DownloadWithExportRequestTest.testDownloadDatasetUsingFegaExportRequestAndVerifyResults();
+  }
+}
