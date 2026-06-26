60607f825f69a0101615825db07b0657b69c291e
Parisa <parisa.tejari@uib.no>
2026-06-26 14:24:48 +0200

    replace deprecated class

--- services/localega-tsd-proxy/src/main/java/no/elixir/fega/ltp/config/RabbitMQConfig.java
+++ services/localega-tsd-proxy/src/main/java/no/elixir/fega/ltp/config/RabbitMQConfig.java
@@ -8,7 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -89,7 +89,7 @@ public class RabbitMQConfig {
 
   @Bean
   public MessageConverter jsonMessageConverter() {
-    return new Jackson2JsonMessageConverter();
+    return new JacksonJsonMessageConverter();
   }
 
   @Bean
